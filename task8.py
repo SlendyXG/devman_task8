bdaf0fae84e0f2017f24b5b22b1025e23340728b
SlendyXG <105538051+SlendyXG@users.noreply.github.com>
2025-08-07 18:07:00 +0300

    Update task8.py

--- task8.py
+++ task8.py
@@ -9,11 +9,11 @@ import folium
 
 
 def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
+    base_url = 'https://geocode-maps.yandex.ru/1.x'
     response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
+        'geocode': address,
+        'apikey': apikey,
+        'format': 'json',
     })
     response.raise_for_status()
     found_places = response.json()['response']['GeoObjectCollection']['featureMember']
@@ -22,38 +22,33 @@ def fetch_coordinates(apikey, address):
         return None
 
     most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(' ')
     return lon, lat
 
 
 def new_structure(apikey, coffee):
-    coords = fetch_coordinates(apikey, address = input("Где вы находитесь?"))
-    print("Ваши координаты:", coords)
+    coords = fetch_coordinates(apikey, address = input('Где вы находитесь?'))
     your_longitude = coords[0]
     your_latitude = coords[1]
     new_coffee = []
 
     for coffee_shop in coffee:
-        title = coffee_shop["Name"]
-        longitude = coffee_shop["geoData"]["coordinates"][0]
-        latitude = coffee_shop["geoData"]["coordinates"][1]
+        title = coffee_shop['Name']
+        longitude = coffee_shop['geoData']['coordinates'][0]
+        latitude = coffee_shop['geoData']['coordinates'][1]
         dist = distance.distance((latitude, longitude), (your_latitude, your_longitude)).km
         new_coffee.append({
-            "title": title,
-            "distance": dist,
-            "longitude": longitude,
-            "latitude": latitude
+            'title': title,
+            'distance': dist,
+            'longitude': longitude,
+            'latitude': latitude
         })
 
     five_closest_coffee(new_coffee=new_coffee, your_longitude=your_longitude, your_latitude=your_latitude)
 
 
 def five_closest_coffee(new_coffee, your_longitude, your_latitude):
-    def get_min_distance(coffee_shop):
-        return coffee_shop['distance']
-
-    sorted_coffee = sorted(new_coffee, key=get_min_distance)
-    closest_five = sorted_coffee[:5]
+    closest_five = sorted(new_coffee, key=lambda coffee: coffee['distance'])[:5]
     create_map(your_longitude=your_longitude, your_latitude=your_latitude, closest_five=closest_five)
 
 
@@ -67,17 +62,17 @@ def create_map(your_longitude, your_latitude, closest_five):
             icon=folium.Icon(color='red')
         ).add_to(m)
 
-    m.save("coffee_map.html")
+    m.save('coffee_map.html')
 
 
 def main():
-    with open("coffee.json", "r") as my_file:
+    with open('coffee.json', 'r', encoding='windows-1251') as my_file:
         coffee_json = my_file.read()
     coffee = json.loads(coffee_json)
     load_dotenv()
-    apikey = os.getenv("APIKEY")
+    apikey = os.getenv('APIKEY')
     new_structure(apikey=apikey, coffee=coffee)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
